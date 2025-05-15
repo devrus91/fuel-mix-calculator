e2890ce6c7c188d218a68a1b0614008dbb0f5b85
Федосеенко Руслан Юрьевич <RFedoseenko@IBS.RU>
2025-05-15 10:22:25 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,2 @@
-﻿const CACHE_NAME = 'mix-calculator-v1';
-const urlsToCache = ['/fuel-mix-calculator/', '/fuel-mix-calculator/static/js/main.02e316e3.js', '/fuel-mix-calculator/static/css/main.e6c13ad2.css'];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
-});
+self.addEventListener("install",(e=>{e.waitUntil(caches.open("my-pwa-cache-v1").then((e=>e.addAll([]))))})),self.addEventListener("fetch",(e=>{e.respondWith(caches.match(e.request).then((t=>t||fetch(e.request))))}));
+//# sourceMappingURL=service-worker.js.map
